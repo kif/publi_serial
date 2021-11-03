d99d06824aed5d017b999ee308f779a9a86febb0
Jerome Kieffer <jerome.kieffer@esrf.fr>
2021-11-03 14:21:38 +0100

    couple of images

---
+++ fig1.py
@@ -0,0 +1,73 @@
+import numpy
+from matplotlib.pyplot import subplots, colorbar
+import pyFAI
+from pyFAI.test.utilstest import UtilsTest
+import fabio
+from matplotlib.colors import LogNorm
+import scipy.optimize
+
+
+img = UtilsTest.getimage("Pilatus6M.cbf")
+geo =  UtilsTest.getimage("Pilatus6M.poni")
+method = ("no", "csr", "cython")
+npt = 500
+targets = [80, 160]#, 240]
+ai = pyFAI.load(geo)
+fimg = fabio.open(img)
+msk = fimg.data<=0
+fixed = fimg.data.copy()
+fixed[msk] = 1 
+fig,ax = subplots(2,2, figsize=(12,8))
+fig.tight_layout(pad=3.0)
+ln = LogNorm(1, fimg.data.max())
+mimg = ax[0,0].imshow(fixed, norm=ln, interpolation="bicubic")
+ax[0,0].set_title("a) MX diffraction frame")
+colorbar(mimg, ax=ax[0,0])
+p0 = ai.sigma_clip_ng(fimg.data, npt, unit="r_mm", method=method, error_model="poisson", thres=0, max_iter=0,)
+p1 = ai.sigma_clip_ng(fimg.data, npt, unit="r_mm", method=method, error_model="poisson", thres=0, max_iter=1,)
+a0 = ai.sigma_clip_ng(fimg.data, npt, unit="r_mm", method=method, error_model="azimuthal", thres=0, max_iter=0,)
+a1 = ai.sigma_clip_ng(fimg.data, npt, unit="r_mm", method=method, error_model="azimuthal", thres=0, max_iter=1,)
+ax[0,1].plot(a0.radial, a0.intensity, label=r"Average")
+ax[0,1].plot(a1.radial, a1.intensity, label=r"Average after clipping")
+ax[0,1].legend()
+ax[0,1].set_xlabel(a0.unit.label)
+ax[0,1].set_ylabel("Intensity (count)")
+ax[0,1].set_title("b) Azimuthal averaging ")
+
+ax[1,0].plot(a0.radial, numpy.sqrt(a0.sum_variance/a0.sum_normalization), alpha=0.7,label=r"Azimuthal deviation")
+ax[1,0].plot(p0.radial, numpy.sqrt(p0.sum_variance/p0.sum_normalization), alpha=0.7,label=r"Poissonian noise") 
+ax[1,0].plot(a0.radial, numpy.sqrt(a1.sum_variance/a1.sum_normalization), alpha=0.7,label=r"Azimuthal deviation after clipping")
+ax[1,0].plot(p0.radial, numpy.sqrt(p1.sum_variance/p1.sum_normalization), alpha=0.7,label=r"Poissonian noise after clipping") 
+ax[1,0].set_xlabel(a0.unit.label)
+ax[1,0].set_ylabel("Error (count)")
+ax[1,0].set_ylim(0, 20)
+ax[1,0].set_title("c) Uncertainties measured")
+ax[1,0].legend()
+
+ax[1,1].set_ylabel("Count")
+ax[1,1].set_xlabel("Intensity")
+ax[1,1].set_title(f"d) Histogram of pixel intensities in a ring")
+
+def gaussian(x, h, c, s):
+    return h*numpy.exp(-(x-c)**2/(2*s*s))
+
+for target in targets:
+    idx = numpy.argmin(abs(target-p0.radial))    
+    key = list(ai.engines.keys())[0]
+    csr = ai.engines[key].engine.lut
+    values = fimg.data.ravel()[csr[1][csr[2][idx]:csr[2][idx+1]]]
+    ax[1,1].hist(values, 42, range=(-1, 40), label=f"ring @ r={target} mm", alpha=0.7)
+    values = values[values>=0]
+    v,c = numpy.histogram(values, values.max())
+    c = numpy.argmax(v)
+    s=1
+    h = v.max()
+    x = numpy.arange(values.max())+0.5 
+    res = scipy.optimize.curve_fit(gaussian, x, v, [h,c,s])[0]
+    ax[1,1].plot(x, gaussian(x, *res), label=r"gauss($\mu=$%.2f, $\sigma=$%.2f)"%(res[1], res[2]))
+ax[1,1].set_xlim(-1, 40)
+ax[1,1].legend()
+
+fig.show()
+fig.savefig("fig1.eps")
+input("Finished !")
