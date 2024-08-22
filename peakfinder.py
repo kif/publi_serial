7b0e6cdac4b90d9e4199a9aca708de27e7ef2904
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-08-22 14:50:54 +0200

    update peak/ring

--- peakfinder.py
+++ peakfinder.py
@@ -19,6 +19,31 @@ from pyFAI.ext.bilinear import Bilinear
 pyfai_color = "limegreen"
 onda_color = "orange"
 
+def parse_stream(fname):
+    indexed = []
+    with open(fname) as f:
+        started = False
+        for line in f:
+            if line.startswith("Reflections measured after indexing"):
+                started = True
+            if started:
+                if line.startswith("   h    k    l          I   sigma(I)       peak background  fs/px  ss/px panel"):
+                    continue
+                elif line.startswith("End of reflections"):
+                    started = False
+                else:
+                    words = line.split()
+                    x, y = words[-3:-1]
+                    try:
+                        indexed.append((float(x), float(y)))
+                    except Exception as e:
+                        print(e)
+    return numpy.array(indexed)
+
+indexed = parse_stream("pilatus6m.stream")
+
+
+
 # Installation of a local copy of the Cython-bound peakfinder8
 targeturl = "https://github.com/kif/peakfinder8"
 targetdir = posixpath.split(targeturl)[-1]
@@ -139,6 +164,7 @@ reh = pf.peakfinder(**kwargs_hy)
 ax.plot(reh["pos1"], reh["pos0"], "1", color="red", label="pyFAI-hybrid")
 ax.plot(res["pos1"], res["pos0"], "1", color=pyfai_color, label="pyFAI")
 ax.plot(res1[0], res1[1], "2", color=onda_color, label="Onda")
+#ax.plot(indexed[:,0], indexed[:,1], ",", color="white", label="Xgandalf")
 
 
 ax.legend()
@@ -146,23 +172,40 @@ fig.savefig("peakfinder.eps")
 fig.savefig("peakfinder.png")
 fig.show()
 
+# peaks Valid, within 2 pixels from indexed: 
+pyFAI_valid = ((numpy.subtract.outer(res["pos1"],indexed[:,0])**2+numpy.subtract.outer(res["pos0"],indexed[:,1])**2).min(axis=-1)<=4)
+onda_valid = ((numpy.subtract.outer(res1[0],indexed[:,0])**2+numpy.subtract.outer(res1[1],indexed[:,1])**2).min(axis=-1)<=4)
+
 print("# Histogram")
 fig, ax = subplots(figsize=(12, 8))
 
-rmax = 44
+rmax = 45
 interp = Bilinear(r2d)
 r_ch = [interp(i) for i in zip(res1[1], res1[0])]
 r_py = [interp(i) for i in zip(res["pos0"], res["pos1"])]
+v_ch = [interp(i) for i in zip(numpy.array(res1[1])[onda_valid], numpy.array(res1[0])[onda_valid])]
+v_py = [interp(i) for i in zip(res["pos0"][pyFAI_valid], res["pos1"][pyFAI_valid])]
+
 # ax.hist(r_py, rmax+1, range=(0, rmax), label="pyFAI", alpha=0.8)
 # ax.hist(r_ch, rmax+1, range=(0, rmax), label="Cheetah", alpha=0.8)
 hpy = numpy.histogram(r_py, rmax + 1, range=(0, rmax))
 hch = numpy.histogram(r_ch, rmax + 1, range=(0, rmax))
-ax.plot(0.5 * (hpy[1][1:] + hpy[1][:-1]), hpy[0], "-", color=pyfai_color, label="pyFAI")
-ax.plot(0.5 * (hch[1][1:] + hch[1][:-1]), hch[0], "-", color=onda_color, label="Onda")
+vpy = numpy.histogram(v_py, rmax + 1, range=(0, rmax))
+vch = numpy.histogram(v_ch, rmax + 1, range=(0, rmax))
+
+#ax.plot(0.5 * (hpy[1][1:] + hpy[1][:-1]), hpy[0], "-", color=pyfai_color, label="pyFAI all peaks")
+#ax.plot(0.5 * (vpy[1][1:] + vpy[1][:-1]), vpy[0], "--", color=pyfai_color, label="pyFAI indexed peaks")
+#ax.plot(0.5 * (hch[1][1:] + hch[1][:-1]), hch[0], "-", color=onda_color, label="Onda all peaks")
+#ax.plot(0.5 * (vch[1][1:] + vch[1][:-1]), vch[0], "--", color=onda_color, label="Onda indexed peaks")
+# Rebin histogram by factor 2
+ax.plot(hpy[1][1::2], hpy[0].reshape(-1,2).sum(axis=-1),"-", color=pyfai_color, label="pyFAI all peaks")
+ax.plot(vpy[1][1::2], vpy[0].reshape(-1,2).sum(axis=-1), "--", color=pyfai_color, label="pyFAI indexed peaks")
+ax.plot(hch[1][1::2], hch[0].reshape(-1,2).sum(axis=-1), "-", color=onda_color, label="Onda all peaks")
+ax.plot(vch[1][1::2], vch[0].reshape(-1,2).sum(axis=-1), "--", color=onda_color, label="Onda indexed peaks")
 # ax.set_xlabel(int1d.unit.label)
 ax.set_xlabel("Resolution $d$-spacing ($\\AA$)")
-ax.set_ylabel("Number of Bragg peaks")
-ax.set_title("Density of Bragg peaks per ring")
+ax.set_ylabel("Number of peaks per ring")
+ax.set_title("Density of peaks per ring")
 ax.legend()
 #
 q1 = ax.get_xticks()
