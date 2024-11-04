1910ec2340521454689aec3cffee3d36fc8dcdd2
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-11-04 16:40:56 +0100

    superimpose the compression ratio measured

--- fig_Rfree.py
+++ fig_Rfree.py
@@ -5,14 +5,26 @@ from matplotlib.pyplot import subplots
 labels = ["initial", "sparse 0.8$\\sigma$","sparse 1.0$\\sigma$","sparse 1.4$\\sigma$","sparse 2.0$\\sigma$"]
 Rwork = [29.11,29.98,30.03,30.61, 31.23]
 Rfree = [34.41,34.58,35.15,35.70,36.94]
+compression = [1.0,
+ 1.3360896819151762,
+ 1.6289966404472827,
+ 2.6298891396823607,
+ 5.856469946283841]
 
 fig,ax=subplots()
-ax.plot(Rwork, label="R$_{work}$")
-ax.plot(Rfree, label="R$_{free}$")
+lns1 = ax.plot(Rwork, label="R$_{work}$ (%)")
+lns2 = ax.plot(Rfree, label="R$_{free}$ (%)")
 ax.set_xticks(range(5), labels)
 ax.set_ylabel("R-factor (%)")
 ax.set_ylim(25, 40)
-ax.legend()
+ax2 = ax.twinx()
+lns3 = ax2.plot(compression, "tab:green", label=r"Compression ratio ($\times$)")
+ax2.set_ylabel(r"Compression ratio ($\times$)")
+
+# added these three lines
+lns = lns1+lns2+lns3
+labs = [l.get_label() for l in lns]
+ax.legend(lns, labs, loc=0)
 fig.savefig("fig_Rfree.eps")
 fig.savefig("fig_Rfree.png")
 fig.show()
